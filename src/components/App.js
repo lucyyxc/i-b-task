d3609dda5b10b00db5c53b9ad1f625115dc41dfa
TrevBo25 <trevorbrown25@gmail.com>
2020-06-08 13:29:04 -0500

    Created footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import Nav from './Nav';
+import Footer from './Footer';
 
 const App = () => (
   <div className="App">
     <Nav />
+    <Footer />
   </div>
 );
 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+    <div className="Footer">
+      <a href="http://instagram.com/theindependentbride" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+      <a href="https://www.pinterest.com/theindependentbride/" target="_blank" rel="noopener noreferrer"><i className="fab fa-pinterest"></i></a>
+      <a href="https://www.facebook.com/the.independent.bride.tribe" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+    </div>
+);
+
+export default Footer;
