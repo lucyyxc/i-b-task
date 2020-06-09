d55b3e13632b3cce78f011639ef52bee4d759d3e
TrevBo25 <trevorbrown25@gmail.com>
2020-06-09 13:00:10 -0500

    Created search field

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
 import Title from './Title';
+import Search from './Search';
 
 const App = () => (
   <div className="App">
     <Nav />
     <Title />
     <Footer />
+    <Search />
   </div>
 );
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Search = () => (
+  <div className="Search">
+    <div className="search-content">
+      <i class="fas fa-user-plus"></i>
+      <i class="fas fa-plus-circle"></i>
+      <i class="far fa-question-circle"></i>
+      <div className="search-input">
+        <i class="fas fa-search"></i>
+        <input placeholder="search"/>
+      </div>
+    </div>
+  </div>
+);
+
+export default Search;
