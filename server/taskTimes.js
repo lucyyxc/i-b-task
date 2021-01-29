6e6ef6a667a7db77cc1df947216b2a794a975495
Trevor Brown <trevorbrown25@gmail.com>
2021-01-28 22:15:09 -0600

    added all tasks to create tasks sql statement

---
+++ server/taskTimes.js
@@ -0,0 +1,1036 @@
+// This is just for the dates of the task, this will not change the task names.
+
+export const tastTimes = [
+  {
+    "Share your engagement news!": {
+      startDates: 365,
+      endDatse: 362,
+    },
+  },
+  {
+    "Decide if you would like to have an engagement party": {
+      startDates: 364,
+      endDatse: 357,
+    },
+  },
+  {
+    "Create rough draft guest list": {
+      startDates: 363,
+      endDatse: 359,
+    },
+  },
+  {
+    "Create budget": {
+      startDates: 362,
+      endDatse: 338,
+    },
+  },
+  {
+    "Get the email address with your new last name": {
+      startDates: 361,
+      endDatse: 361,
+    },
+  },
+  {
+    "Discuss potential wedding date options": {
+      startDates: 361,
+      endDatse: 358,
+    },
+  },
+  {
+    "Determine wedding location": {
+      startDates: 360,
+      endDatse: 337,
+    },
+  },
+  {
+    "Create vision board": {
+      startDates: 359,
+      endDatse: 352,
+    },
+  },
+  {
+    "Create wedding specific email address": {
+      startDates: 358,
+      endDatse: 358,
+    },
+  },
+  {
+    "Book reception venue ": {
+      startDates: 360,
+      endDatse: 327,
+    },
+  },
+  {
+    "Book ceremony venue": {
+      startDates: 357,
+      endDatse: 305,
+    },
+  },
+  {
+    "Create wedding documents folder": {
+      startDates: 333,
+      endDatse: 331,
+    },
+  },
+  {
+    "Create wedding website": {
+      startDates: 242,
+      endDatse: 210,
+    },
+  },
+  {
+    "Finalize guest list": {
+      startDates: 328,
+      endDatse: 321,
+    },
+  },
+  {
+    "Decide on your bridesmaids and groomsmen": {
+      startDates: 350,
+      endDatse: 326,
+    },
+  },
+  {
+    "Decide on your remaining wedding party": {
+      startDates: 325,
+      endDatse: 311,
+    },
+  },
+  {
+    "Book caterer": {
+      startDates: 324,
+      endDatse: 296,
+    },
+  },
+  {
+    "Book videographer": {
+      startDates: 311,
+      endDatse: 291,
+    },
+  },
+  {
+    "Purchase wedding dress": {
+      startDates: 351,
+      endDatse: 298,
+    },
+  },
+  {
+    "Book photographer": {
+      startDates: 300,
+      endDatse: 274,
+    },
+  },
+  {
+    "Book florist": {
+      startDates: 297,
+      endDatse: 281,
+    },
+  },
+  {
+    "Book DJ/musical guest": {
+      startDates: 293,
+      endDatse: 264,
+    },
+  },
+  {
+    "Book hair stylist ": {
+      startDates: 304,
+      endDatse: 276,
+    },
+  },
+  {
+    "Book makeup artist ": {
+      startDates: 304,
+      endDatse: 276,
+    },
+  },
+  {
+    "Secure hotel room blocks": {
+      startDates: 283,
+      endDatse: 271,
+    },
+  },
+  {
+    "Determine save the date design ": {
+      startDates: 280,
+      endDatse: 266,
+    },
+  },
+  {
+    "Create gift registry ": {
+      startDates: 267,
+      endDatse: 246,
+    },
+  },
+  {
+    "Book photo booth": {
+      startDates: 316,
+      endDatse: 301,
+    },
+  },
+  {
+    "Collect guest mailing addresses": {
+      startDates: 267,
+      endDatse: 253,
+    },
+  },
+  {
+    "Decide the wedding day photography schedule": {
+      startDates: 267,
+      endDatse: 254,
+    },
+  },
+  {
+    "Decide on bridal shower date options": {
+      startDates: 324,
+      endDatse: 317,
+    },
+  },
+  {
+    "Decide key Bachelorette party details": {
+      startDates: 318,
+      endDatse: 312,
+    },
+  },
+  {
+    "Purchase save the date cards": {
+      startDates: 265,
+      endDatse: 258,
+    },
+  },
+  {
+    "Purchase bride's wedding ring": {
+      startDates: 259,
+      endDatse: 232,
+    },
+  },
+  {
+    "Purchase groom's wedding ring": {
+      startDates: 259,
+      endDatse: 232,
+    },
+  },
+  {
+    "Purchase insurance for wedding rings": {
+      startDates: 230,
+      endDatse: 223,
+    },
+  },
+  {
+    "Schedule engagement photos ": {
+      startDates: 248,
+      endDatse: 243,
+    },
+  },
+  {
+    "Book musicians for the ceremony": {
+      startDates: 239,
+      endDatse: 209,
+    },
+  },
+  {
+    "Book day of wedding planner/coordinator": {
+      startDates: 257,
+      endDatse: 236,
+    },
+  },
+  {
+    "Book cake baker": {
+      startDates: 252,
+      endDatse: 231,
+    },
+  },
+  {
+    "Buy stamps for save the date/invitations": {
+      startDates: 231,
+      endDatse: 225,
+    },
+  },
+  {
+    "Create transportation plan for wedding party": {
+      startDates: 235,
+      endDatse: 216,
+    },
+  },
+  {
+    "Secure wedding officiant": {
+      startDates: 227,
+      endDatse: 209,
+    },
+  },
+  {
+    "Book wedding night hotel room": {
+      startDates: 270,
+      endDatse: 266,
+    },
+  },
+  {
+    "Book rehearsal dinner venue": {
+      startDates: 221,
+      endDatse: 197,
+    },
+  },
+  {
+    "Buy or rent groom's wedding suit/tux": {
+      startDates: 218,
+      endDatse: 190,
+    },
+  },
+  {
+    "Create a weather plan 'B'": {
+      startDates: 215,
+      endDatse: 188,
+    },
+  },
+  {
+    "Determine wedding reception decorations": {
+      startDates: 212,
+      endDatse: 167,
+    },
+  },
+  {
+    "Send save the dates": {
+      startDates: 209,
+      endDatse: 204,
+    },
+  },
+  {
+    "Order wedding invitations": {
+      startDates: 208,
+      endDatse: 195,
+    },
+  },
+  {
+    "Determine cake style/flavor": {
+      startDates: 200,
+      endDatse: 175,
+    },
+  },
+  {
+    "Buy wedding shoes": {
+      startDates: 208,
+      endDatse: 180,
+    },
+  },
+  {
+    "Determine wedding day hair style": {
+      startDates: 146,
+      endDatse: 140,
+    },
+  },
+  {
+    "Determine wedding day makeup": {
+      startDates: 146,
+      endDatse: 140,
+    },
+  },
+  {
+    "Book/meet with rental and lighting pros": {
+      startDates: 192,
+      endDatse: 164,
+    },
+  },
+  {
+    "Purchase flower girl dress": {
+      startDates: 195,
+      endDatse: 181,
+    },
+  },
+  {
+    "Purchase flower girl accessories ": {
+      startDates: 187,
+      endDatse: 176,
+    },
+  },
+  {
+    "Create guest experience flow ": {
+      startDates: 190,
+      endDatse: 166,
+    },
+  },
+  {
+    "Determine bridesmaid dresses/share with wedding party": {
+      startDates: 180,
+      endDatse: 154,
+    },
+  },
+  {
+    "Arrange guest shuttle, parking, and/or valet at reception venue": {
+      startDates: 177,
+      endDatse: 157,
+    },
+  },
+  {
+    "Order rental items": {
+      startDates: 175,
+      endDatse: 168,
+    },
+  },
+  {
+    "Start premarital counseling ": {
+      startDates: 172,
+      endDatse: 158,
+    },
+  },
+  {
+    "Determine bridesmaids shoes": {
+      startDates: 167,
+      endDatse: 150,
+    },
+  },
+  {
+    "Plan welcome party": {
+      startDates: 165,
+      endDatse: 139,
+    },
+  },
+  {
+    "Plan day after wedding ": {
+      startDates: 162,
+      endDatse: 131,
+    },
+  },
+  {
+    "Schedule wedding dress alteration/fitting": {
+      startDates: 163,
+      endDatse: 163,
+    },
+  },
+  {
+    "Purchase headpiece/veil": {
+      startDates: 157,
+      endDatse: 143,
+    },
+  },
+  {
+    "Create guest list for rehearsal dinner": {
+      startDates: 156,
+      endDatse: 153,
+    },
+  },
+  {
+    "Purchase rehearsal dinner invitations": {
+      startDates: 153,
+      endDatse: 148,
+    },
+  },
+  {
+    "Prep wedding invitations": {
+      startDates: 150,
+      endDatse: 147,
+    },
+  },
+  {
+    "Confirm wedding guest RSVP names": {
+      startDates: 148,
+      endDatse: 147,
+    },
+  },
+  {
+    "Send wedding invitations": {
+      startDates: 146,
+      endDatse: 146,
+    },
+  },
+  {
+    "Determine ceremony decorations": {
+      startDates: 143,
+      endDatse: 123,
+    },
+  },
+  {
+    "Monitor RSVPs": {
+      startDates: 139,
+      endDatse: 75,
+    },
+  },
+  {
+    "Order wedding favors": {
+      startDates: 140,
+      endDatse: 122,
+    },
+  },
+  {
+    "Schedule wedding day hair trial": {
+      startDates: 138,
+      endDatse: 138,
+    },
+  },
+  {
+    "Schedule wedding day makeup trial": {
+      startDates: 137,
+      endDatse: 137,
+    },
+  },
+  {
+    "Create displays and signage for reception": {
+      startDates: 136,
+      endDatse: 112,
+    },
+  },
+  {
+    "Purchase cake topper": {
+      startDates: 139,
+      endDatse: 133,
+    },
+  },
+  {
+    "Buy under-the-dress essentials": {
+      startDates: 224,
+      endDatse: 219,
+    },
+  },
+  {
+    "Decide on guest book": {
+      startDates: 130,
+      endDatse: 116,
+    },
+  },
+  {
+    "Book dance lessons ": {
+      startDates: 128,
+      endDatse: 121,
+    },
+  },
+  {
+    "Determine suit/tux for groomsmen/share with wedding party": {
+      startDates: 126,
+      endDatse: 120,
+    },
+  },
+  {
+    "Send rehearsal dinner invitations": {
+      startDates: 122,
+      endDatse: 115,
+    },
+  },
+  {
+    "Purchase bridesmaid gifts": {
+      startDates: 121,
+      endDatse: 100,
+    },
+  },
+  {
+    "Purchase groomsmen gifts": {
+      startDates: 120,
+      endDatse: 114,
+    },
+  },
+  {
+    "Buy gifts and/or activities for the flower girl(s)/ring bearer(s)": {
+      startDates: 111,
+      endDatse: 105,
+    },
+  },
+  {
+    "Purchase gifts for other VIP's": {
+      startDates: 114,
+      endDatse: 108,
+    },
+  },
+  {
+    "Determine jewelry/accessories for wedding day": {
+      startDates: 116,
+      endDatse: 102,
+    },
+  },
+  {
+    "Determine groom's accessories for wedding day": {
+      startDates: 115,
+      endDatse: 109,
+    },
+  },
+  {
+    "Finalize flowers with florist": {
+      startDates: 113,
+      endDatse: 99,
+    },
+  },
+  {
+    "Determine bridesmaids bouquets": {
+      startDates: 105,
+      endDatse: 99,
+    },
+  },
+  {
+    "Determine boutonnieres and corsages for VIPs": {
+      startDates: 110,
+      endDatse: 105,
+    },
+  },
+  {
+    "Order flower petals & basket(s) for flower girl(s)": {
+      startDates: 98,
+      endDatse: 98,
+    },
+  },
+  {
+    "Purchase ring bearer pillow(s)": {
+      startDates: 108,
+      endDatse: 101,
+    },
+  },
+  {
+    "Create playlist for wedding ceremony": {
+      startDates: 101,
+      endDatse: 85,
+    },
+  },
+  {
+    "Determine readings for wedding ceremony": {
+      startDates: 98,
+      endDatse: 84,
+    },
+  },
+  {
+    "Write/determine vows": {
+      startDates: 104,
+      endDatse: 77,
+    },
+  },
+  {
+    "Determine how parents will walk into the ceremony": {
+      startDates: 102,
+      endDatse: 102,
+    },
+  },
+  {
+    "Determine grandparents ushers": {
+      startDates: 101,
+      endDatse: 101,
+    },
+  },
+  {
+    "Plan big exit from the ceremony": {
+      startDates: 100,
+      endDatse: 93,
+    },
+  },
+  {
+    "Schedule and meet with officiant": {
+      startDates: 100,
+      endDatse: 87,
+    },
+  },
+  {
+    "Create full ceremony plan and share as needed": {
+      startDates: 99,
+      endDatse: 72,
+    },
+  },
+  {
+    "Finalize wedding reception menu": {
+      startDates: 95,
+      endDatse: 89,
+    },
+  },
+  {
+    "Plan after party": {
+      startDates: 88,
+      endDatse: 75,
+    },
+  },
+  {
+    "Plan days leading up to wedding": {
+      startDates: 81,
+      endDatse: 75,
+    },
+  },
+  {
+    "Determine cocktails/wine for reception": {
+      startDates: 87,
+      endDatse: 81,
+    },
+  },
+  {
+    "Purchase bridal party getting ready outfits": {
+      startDates: 85,
+      endDatse: 79,
+    },
+  },
+  {
+    "Determine wedding photo locations/create shot list": {
+      startDates: 77,
+      endDatse: 71,
+    },
+  },
+  {
+    "Create wedding weekend schedules for VIPs": {
+      startDates: 74,
+      endDatse: 61,
+    },
+  },
+  {
+    "Determine welcome gift": {
+      startDates: 80,
+      endDatse: 74,
+    },
+  },
+  {
+    "Make any additional hair appointments for days prior ": {
+      startDates: 78,
+      endDatse: 78,
+    },
+  },
+  {
+    "Create list of must play and do not play songs for DJ": {
+      startDates: 77,
+      endDatse: 70,
+    },
+  },
+  {
+    "Create photo slideshow for reception": {
+      startDates: 75,
+      endDatse: 62,
+    },
+  },
+  {
+    "Create name tags for reception": {
+      startDates: 73,
+      endDatse: 67,
+    },
+  },
+  {
+    "Request time off work for wedding and honeymoon": {
+      startDates: 72,
+      endDatse: 72,
+    },
+  },
+  {
+    "Request bridesmaids to choose wedding hair style": {
+      startDates: 71,
+      endDatse: 58,
+    },
+  },
+  {
+    "Purchase cologne for wedding day": {
+      startDates: 70,
+      endDatse: 62,
+    },
+  },
+  {
+    "Purchase perfume for wedding day": {
+      startDates: 70,
+      endDatse: 62,
+    },
+  },
+  {
+    "Create baskets for wedding reception restrooms": {
+      startDates: 51,
+      endDatse: 38,
+    },
+  },
+  {
+    "Build wedding day 'emergency' kit": {
+      startDates: 50,
+      endDatse: 37,
+    },
+  },
+  {
+    "Purchase rehearsal dinner outfit": {
+      startDates: 66,
+      endDatse: 52,
+    },
+  },
+  {
+    "Choose something blue, borrowed, old and new": {
+      startDates: 65,
+      endDatse: 58,
+    },
+  },
+  {
+    "Choose prayer/welcome toast ": {
+      startDates: 63,
+      endDatse: 57,
+    },
+  },
+  {
+    "Make nail and spa appointments": {
+      startDates: 61,
+      endDatse: 60,
+    },
+  },
+  {
+    "Design wedding program for ceremony & print": {
+      startDates: 60,
+      endDatse: 53,
+    },
+  },
+  {
+    "Determine photo sharing app to gather guests photos": {
+      startDates: 58,
+      endDatse: 51,
+    },
+  },
+  {
+    "Create plan 'B' communication plan for wedding day ": {
+      startDates: 57,
+      endDatse: 50,
+    },
+  },
+  {
+    "Create schedule for toasts at wedding reception": {
+      startDates: 55,
+      endDatse: 55,
+    },
+  },
+  {
+    "Write rehearsal and wedding reception toast": {
+      startDates: 54,
+      endDatse: 40,
+    },
+  },
+  {
+    "Create hashtag": {
+      startDates: 52,
+      endDatse: 45,
+    },
+  },
+  {
+    "Write VIP thank yous": {
+      startDates: 57,
+      endDatse: 45,
+    },
+  },
+  {
+    "Create signage ": {
+      startDates: 47,
+      endDatse: 17,
+    },
+  },
+  {
+    "Order name cards and table numbers": {
+      startDates: 37,
+      endDatse: 31,
+    },
+  },
+  {
+    "Put together outfits for days before and after wedding": {
+      startDates: 40,
+      endDatse: 29,
+    },
+  },
+  {
+    "Create rehearsal dinner menu": {
+      startDates: 42,
+      endDatse: 35,
+    },
+  },
+  {
+    "Create traveling guest google form": {
+      startDates: 41,
+      endDatse: 34,
+    },
+  },
+  {
+    "Create pre-wedding ceremony menu ": {
+      startDates: 38,
+      endDatse: 31,
+    },
+  },
+  {
+    "Appoint gift and card retriever ": {
+      startDates: 33,
+      endDatse: 26,
+    },
+  },
+  {
+    "Contact people who have not RSVP'd": {
+      startDates: 31,
+      endDatse: 30,
+    },
+  },
+  {
+    "Break in shoes for wedding day and purchase padding": {
+      startDates: 31,
+      endDatse: 10,
+    },
+  },
+  {
+    "Design/print wedding reception menu": {
+      startDates: 30,
+      endDatse: 28,
+    },
+  },
+  {
+    "Arrange/communicate transportation for VIP guests": {
+      startDates: 29,
+      endDatse: 27,
+    },
+  },
+  {
+    "Ask photographer, DJ, etc. for food preference": {
+      startDates: 29,
+      endDatse: 27,
+    },
+  },
+  {
+    "Create seating chart for the wedding reception": {
+      startDates: 28,
+      endDatse: 14,
+    },
+  },
+  {
+    "Create entree indicators": {
+      startDates: 27,
+      endDatse: 21,
+    },
+  },
+  {
+    "Get marriage license": {
+      startDates: 26,
+      endDatse: 22,
+    },
+  },
+  {
+    "Get hand steamer for dresses": {
+      startDates: 36,
+      endDatse: 32,
+    },
+  },
+  {
+    "Send email for wedding rehearsal with special instructions": {
+      startDates: 25,
+      endDatse: 22,
+    },
+  },
+  {
+    "Send extended family email with special instructions ": {
+      startDates: 25,
+      endDatse: 22,
+    },
+  },
+  {
+    "Confirm payment with vendors": {
+      startDates: 19,
+      endDatse: 17,
+    },
+  },
+  {
+    "Send your day of schedule to all vendors": {
+      startDates: 13,
+      endDatse: 11,
+    },
+  },
+  {
+    "Appoint person to check into hotel": {
+      startDates: 21,
+      endDatse: 21,
+    },
+  },
+  {
+    "Gather getting ready magazines, books, music, etc.": {
+      startDates: 21,
+      endDatse: 14,
+    },
+  },
+  {
+    "Send email to guests with all important details  ": {
+      startDates: 20,
+      endDatse: 15,
+    },
+  },
+  {
+    "Create packing lists": {
+      startDates: 13,
+      endDatse: 8,
+    },
+  },
+  {
+    "Submit final numbers to caterer": {
+      startDates: 17,
+      endDatse: 14,
+    },
+  },
+  {
+    "Pay vendors": {
+      startDates: 16,
+      endDatse: 14,
+    },
+  },
+  {
+    "Delegate person to make day of payments and deliver tips": {
+      startDates: 15,
+      endDatse: 15,
+    },
+  },
+  {
+    "Write morning of cards to exchange": {
+      startDates: 14,
+      endDatse: 7,
+    },
+  },
+  {
+    "Request early check in": {
+      startDates: 7,
+      endDatse: 7,
+    },
+  },
+  {
+    "Clean engagement ring": {
+      startDates: 6,
+      endDatse: 6,
+    },
+  },
+  {
+    "Pack for wedding night": {
+      startDates: 5,
+      endDatse: 3,
+    },
+  },
+  {
+    "Share a video on how to pin on a boutineer with the wedding party": {
+      startDates: 0,
+      endDatse: 0,
+    },
+  },
+  {
+    "Change relationship status and last name on social media": {
+      startDates: -1,
+      endDatse: -1,
+    },
+  },
+  {
+    "Launch new email address & set up forwarding": {
+      startDates: -3,
+      endDatse: -3,
+    },
+  },
+  {
+    "Get wedding dress cleaned and preserved": {
+      startDates: -4,
+      endDatse: -45,
+    },
+  },
+  {
+    "Change your name legally": {
+      startDates: -25,
+      endDatse: -40,
+    },
+  },
+  {
+    "Send thank you cards": {
+      startDates: -26,
+      endDatse: -56,
+    },
+  },
+  {
+    "Write reviews on wedding vendors": {
+      startDates: -30,
+      endDatse: -44,
+    },
+  },
+  {
+    "Review wedding photos and post favorites": {
+      startDates: -90,
+      endDatse: -104,
+    },
+  },
+  {
+    "Create/order wedding album": {
+      startDates: -100,
+      endDatse: -114,
+    },
+  },
+];
